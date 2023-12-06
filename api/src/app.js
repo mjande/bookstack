ddca9203428feca7f175de6834c61c650c49d0db
mattand <mjande@umich.edu>
2023-12-06 09:56:52 -0500

    Set up server file and connect to mongodb

---
+++ api/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log("Connected to MongoDB.");
+} catch (err) {
+  console.log("Unable to connect to MongoDB.");
+  console.log(err);
+}
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello world!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
