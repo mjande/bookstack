54c3ef8c69dd7449eda7ccd5556d44721ad43f9e
mattand <mjande@umich.edu>
2023-12-27 17:36:25 -0500

    Add edit user and log out buttons to nav

--- client/src/layout/MainLayout/Banner/Banner.jsx
+++ client/src/layout/MainLayout/Banner/Banner.jsx
@@ -7,20 +7,39 @@ function Banner() {
   // If user is not logged in, buttons should be log in or register
   // If user is logged in, buttons should be edit user or log out
 
+  const isLoggedIn = !!localStorage.getItem("accessToken");
+
   return (
     <nav className={styles.Banner}>
       <Link to="/">Home</Link>
       <div className={styles.rightLinks}>
-        <Link to="/users/login">
-          <Button color="info" variant="outlined">
-            Log In
-          </Button>
-        </Link>
-        <Link to="/users/register">
-          <Button color="info" variant="contained">
-            Register
-          </Button>
-        </Link>
+        {isLoggedIn ? (
+          <>
+            <Link to="/users/edit">
+              <Button color="info" variant="outlined">
+                Edit User
+              </Button>
+            </Link>
+            <Link to="/users/logout">
+              <Button color="info" variant="contained">
+                Log Out
+              </Button>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/users/login">
+              <Button color="info" variant="outlined">
+                Log In
+              </Button>
+            </Link>
+            <Link to="/users/register">
+              <Button color="info" variant="contained">
+                Register
+              </Button>
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
